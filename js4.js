e2b528a190660492ecb70566a192116c6b8210e0
Student <student@RU-005.local>
2016-01-18 16:39:06 -0700

    Bonus 1 and 2 complete

--- js4.js
+++ js4.js
@@ -1,3 +1,4 @@
+//delete for bonus #1
 var victimCount = prompt("How many victims do you wish to enter?")
 
 var names = []
@@ -9,6 +10,15 @@ for (i = 0; i < victimCount; i++) {
 	phoneNumbers.push(prompt("What is their phone number?"))
 	streets.push(prompt("What is their street?"))
 }
+
+// Bonus #1 code
+//  while (cont) {
+// 	names.push(prompt("What is their name?"))
+// 	phoneNumbers.push(prompt("What is their phone number?"))
+// 	streets.push(prompt("What is their street?"))
+// 	cont = confirm("Add another?")
+// }
+
 
 var volunteerCount = prompt("How many volunteers are needed?")
 var volunteerNames = []
@@ -22,3 +32,16 @@ for (i = 0; i < volunteerCount; i++) {
 }
 
 alert("# of persons in need: " + victimCount + "\n# of volunteers: " + volunteerCount + "\nVictims: " + names + "\nVolunteers: " + volunteerNames) 
+// Bonus #2 code
+// var personInNeed = prompt("Who is in need?")
+// var streetInNeed = ""
+// for (i = 0; i < names.length; i++) {
+// 	if (personInNeed === names[i]) {
+// 		streetInNeed = streets[i]
+// 	}
+// }
+// for (n = 0; n < streets.length; n++) {
+// 	if (streetInNeed === volunteerStreets[n]) {
+// 		alert("The closest volunteer is " + volunteerNames[n])
+// 	}
+// }
